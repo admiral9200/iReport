bd2387b5d36f92c28ef479609970cde13db7f921
Nicholas Wong <lion9200.world@gmail.com>
2023-09-04 04:15:57 +0800

    configured tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./assets/main.css";
 
 const App = () => {
-    return <>
-        App
-    </>
+    return (
+        <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+            <p className="text-3xl text-gray-700 font-bold mb-5">
+                Welcome!
+            </p>
+        </div>
+    )
 }
 
 export default App;
